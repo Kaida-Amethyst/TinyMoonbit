1739f31ab4ac6a5cda3ef88b93128123aea0d10a
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-10-11 15:17:06 +0800

    add two cases

--- runtime.c
+++ runtime.c
@@ -4,3 +4,9 @@
 void print_int(int value) {
   printf("%d\n", value);
 }
+
+void print_double(double value) {
+  printf("%lf\n", value);
+}
+
+// sin, cos, sqrt: no need special runtime functions, just use math.h
